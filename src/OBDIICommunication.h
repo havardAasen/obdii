860385daf84140a86bca2b103cb0be36be6ca188
Håvard F. Aasen <havard.f.aasen@pfft.no>
2022-12-29 19:50:55 +0100

    docs: Wrap code in docstrings with correct commands for Doxygen
    Even if documentation isn't generated with Doxygen, a lot of IDE's
    display docstrings with the same syntax as Doxygen. The code will now
    show properly, without mangling the code part.

--- src/OBDIICommunication.h
+++ src/OBDIICommunication.h
@@ -13,10 +13,11 @@ typedef struct {
 	canid_t rid;
 } OBDIISocket;
 
-/** Open a communication channel to a particular ECU.
+/**
+ * \brief Open a communication channel to a particular ECU.
  *
  * This function binds a CAN socket to a network interface, in order to communicate with a particular ECU.
- * The returned socket uses the ISOTP transport protocol (CAN_ISOTP defined in <linux/can.h>). The ISOTP protocol is implemented
+ * The returned socket uses the ISOTP transport protocol (CAN_ISOTP defined in `<linux/can.h>`). The ISOTP protocol is implemented
  * in a third-party kernel module that must be installed on the system. The module's source is available at https://github.com/hartkopp/can-isotp-modules,
  * which contains instructions for building and installing it.
  *
@@ -24,41 +25,53 @@ typedef struct {
  * The receive ID is the ID the ECU uses to respond. For vehicles that use 11-bit identifiers, ECUs have a transfer ID in the range 0x7E0 to 0x7E7,
  * and they respond with an ID in the range 0x7E8 to 0x7EF (0x08 more than their transfer ID).
  *
- *     OBDIISocket s;
- *     if (OBDIIOpenSocket(&s, "can0", 0x7E0, 0x7E8, 1) < 0) { // Talk to the engine ECU
- *         printf("Error opening socket: %s\n", strerror(errno));
- *     }
+ * \code
+ * OBDIISocket s;
+ * if (OBDIIOpenSocket(&s, "can0", 0x7E0, 0x7E8, 1) < 0) { // Talk to the engine ECU
+ *     printf("Error opening socket: %s\n", strerror(errno));
+ * }
+ * \endcode
  *
  * \param s The `OBDIISocket` struct that will be filled in by the call
  * \param ifname The name of the CAN interface that the socket will be bound to
  * \param tx_id The ID used to address frames to the ECU
  * \param rx_id The ID the ECU will use for response frames
- * \param shared If `1`, the socket will be opened by the OBDII daemon instead of by the calling process. This allows multiple processes to share the same OBDII socket, as only a single process can bind to a particular (interface, transfer ID, receive ID) tuple. If `0`, the calling process will bind the socket, which may fail if another process has already bound a socket for the given parameters.
+ * \param shared If `1`, the socket will be opened by the OBDII daemon instead
+ *               of by the calling process. This allows multiple processes to
+ *               share the same OBDII socket, as only a single process can bind
+ *               to a particular (interface, transfer ID, receive ID) tuple. If
+ *               `0`, the calling process will bind the socket, which may fail
+ *               if another process has already bound a socket for the given
+ *               parameters.
  *
  * \returns 0 on success, -1 on error
  */
 int OBDIIOpenSocket(OBDIISocket *s, const char *ifname, canid_t tx_id, canid_t rx_id, int shared);
 
-/** Close an open socket created with `OBDIIOpenSocket`
+/**
+ * \brief Close an open socket created with `OBDIIOpenSocket()`
  *
- * \param s The socket structure filled in by a call to `OBDIIOpenSocket`
+ * \param s The socket structure filled in by a call to `OBDIIOpenSocket()`
  * 
  * \returns 0 on success, -1 on error
  */
 int OBDIICloseSocket(OBDIISocket *s);
 
-/** Query the car for a particular command and return the response.
+/**
+ * \brief Query the car for a particular command and return the response.
  *
  * This is the main API that clients will interact with. It writes the command's request payload into the socket
  * and reads the response payload, decoding it as an `OBDIIResponse` object.
  *
- *     OBDIIResponse response = OBDIIPerformQuery(&s, OBDIICommands.engineRPMs);
- *     if (response.success) {
- *         printf("%.2f", response.numericValue);
- *     } else {
- *         // Handle error
- *     }
- *     OBDIIResponseFree(&response);
+ * \code
+ * OBDIIResponse response = OBDIIPerformQuery(&s, OBDIICommands.engineRPMs);
+ * if (response.success) {
+ *     printf("%.2f", response.numericValue);
+ * } else {
+ *     // Handle error
+ * }
+ * OBDIIResponseFree(&response);
+ * \endcode
  *
  * \param s The socket used to communicate with the vehicle
  * \param command The command to query the vehicle for
@@ -67,13 +80,16 @@ int OBDIICloseSocket(OBDIISocket *s);
  */
 OBDIIResponse OBDIIPerformQuery(OBDIISocket *s, OBDIICommand *command);
 
-/** Queries the car for the commands it supports.
+/**
+ * \brief Queries the car for the commands it supports.
  *
- *     OBDIICommandSet commands = OBDIIGetSupportedCommands(&s);
- *     if (OBDIICommandSetContainsCommand(&commands, OBDIICommands.engineRPMs)) {
- *         // Query the vehicle
- *     }
- *     OBDIICommandSetFree(&commands);
+ * \code
+ * OBDIICommandSet commands = OBDIIGetSupportedCommands(&s);
+ * if (OBDIICommandSetContainsCommand(&commands, OBDIICommands.engineRPMs)) {
+ *     // Query the vehicle
+ * }
+ * OBDIICommandSetFree(&commands);
+ * \endcode
  */
 OBDIICommandSet OBDIIGetSupportedCommands(OBDIISocket *s);
 
